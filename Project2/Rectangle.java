f472b97cc25386c0bf2dad827125a6aff26d55ea
Nick Sky <GNickSky9@users.noreply.github.com>
2016-05-16 21:21:07 -0400

    Create Rectangle.java

---
+++ Project2/Rectangle.java
@@ -0,0 +1,23 @@
+public class Rectangle
+{
+	public int width;
+	public int height;
+	public int x;
+	public int y;
+	
+	public Rectangle()
+	{
+		width = 50;
+		height = 50;
+		x = 0;
+		y= 0;
+	}
+	
+	public Rectangle(int x, int y)
+	{
+		width = 50;
+		height = 50;
+		this.x = x;
+		this.y = y;
+	}
+}
